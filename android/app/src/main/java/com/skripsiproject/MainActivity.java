10fa205ac0c5e4e77a655dbf9edd9d26f1f0afe8
Aditya Putra <adityaaputraaa98@gmail.com>
2021-06-17 15:19:53 +0700

    change project name and release the app into APK

--- android/app/src/main/java/com/skripsiproject/MainActivity.java
+++ android/app/src/main/java/com/skripsiproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SkripsiProject";
+    return "SPK";
   }
   @Override
    protected ReactActivityDelegate createReactActivityDelegate() {
